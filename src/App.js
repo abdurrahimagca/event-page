d7f1d56e82e145be1000d056988df091f5345ddf
abdurrahimagca <agcaabdurrahim@outlook.com>
2024-11-25 19:30:13 +0300

    w screen

--- src/App.js
+++ src/App.js
@@ -1,69 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Lottie from "react-lottie";
+import animationData from "./animation/waiting.json"; 
 
 function App() {
-  const courses = [
-    {
-      title: "POLİVAGAL TEORİ / İLKELER VE UYGULAMA",
-      level: "seviye bilgisi",
-      trainer: "Deb Dana",
-      duration: "süre",
-      certificate: "sertifika",
-      price: "€32",
-      oldPrice: "€55",
-      specialOffer: true,
-  
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
     },
-    {
-      title: "POLİVAGAL TEORİ / İLKELER VE UYGULAMA",
-      level: "seviye bilgisi",
-      trainer: "Deb Dana",
-      duration: "süre",
-      certificate: "sertifika",
-      price: "€32",
-      oldPrice: "€55",
-      specialOffer: true,
-  
-    },
-  ];
+  };
 
   return (
-    <div className="App">
-      <header className="header">
-        <nav>
-          <ul>
-           <li>
-            <button className='filter-button'>Tümü</button>
-            
-           </li>
-            <li>
-              <button className='filter-button'>Yeni</button>
-            </li>
+    <div className="coming-soon-container">
+      <div className="text-container">
+         <h1>Yakında...</h1>
+      <p>Yakında burada etkinliklerle sizlerleyiz.</p>
 
-          </ul>
-        
-        </nav>
-      </header>
-      <main className="course-list">
-        {courses.map((course, index) => (
-          <div className="course-card" key={index}>
-
-            
-            <h3>{course.title}</h3>
-            <p>{course.level}</p>
-            {course.trainer && (
-              <div className="trainer">
-                <span>{course.trainer}</span>
-              </div>
-            )}
-            <div className="price">
-              {course.oldPrice && <span className="old-price">{course.oldPrice}</span>}
-              <span className="new-price">{course.price}</span>
-            </div>
-            <button className="enroll-button">KAYDOL</button>
-          </div>
-        ))}
-      </main>
+      </div>
+     
+      <Lottie options={defaultOptions} height={200} width={200} />
     </div>
   );
 }
---
+++ src/core/event.js
@@ -0,0 +1,69 @@
+import React from 'react';
+function App() {
+  const courses = [
+    {
+      title: "POLİVAGAL TEORİ / İLKELER VE UYGULAMA",
+      level: "seviye bilgisi",
+      trainer: "Deb Dana",
+      duration: "süre",
+      certificate: "sertifika",
+      price: "€32",
+      oldPrice: "€55",
+      specialOffer: true,
+  
+    },
+    {
+      title: "POLİVAGAL TEORİ / İLKELER VE UYGULAMA",
+      level: "seviye bilgisi",
+      trainer: "Deb Dana",
+      duration: "süre",
+      certificate: "sertifika",
+      price: "€32",
+      oldPrice: "€55",
+      specialOffer: true,
+  
+    },
+  ];
+
+  return (
+    <div className="App">
+      <header className="header">
+        <nav>
+          <ul>
+           <li>
+            <button className='filter-button'>Tümü</button>
+            
+           </li>
+            <li>
+              <button className='filter-button'>Yeni</button>
+            </li>
+
+          </ul>
+        
+        </nav>
+      </header>
+      <main className="course-list">
+        {courses.map((course, index) => (
+          <div className="course-card" key={index}>
+
+            
+            <h3>{course.title}</h3>
+            <p>{course.level}</p>
+            {course.trainer && (
+              <div className="trainer">
+                <span>{course.trainer}</span>
+              </div>
+            )}
+            <div className="price">
+              {course.oldPrice && <span className="old-price">{course.oldPrice}</span>}
+              <span className="new-price">{course.price}</span>
+            </div>
+            <button className="enroll-button">KAYDOL</button>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+}
+
+export default App;
