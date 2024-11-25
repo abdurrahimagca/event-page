ebd18ee6f69f33ebc4d4b71ef8d4dbb95b87528a
abdurrahimagca <agcaabdurrahim@outlook.com>
2024-11-25 18:31:27 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const courses = [
+    {
+      title: "POLİVAGAL TEORİ / İLKELER VE UYGULAMA",
+      level: "seviye bilgisi",
+      trainer: "Deb Dana",
+      duration: "süre",
+      certificate: "sertifika",
+      price: "€32",
+      oldPrice: "€55",
+      specialOffer: true,
+  
+    },
+    {
+      title: "POLİVAGAL TEORİ / İLKELER VE UYGULAMA",
+      level: "seviye bilgisi",
+      trainer: "Deb Dana",
+      duration: "süre",
+      certificate: "sertifika",
+      price: "€32",
+      oldPrice: "€55",
+      specialOffer: true,
+  
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <nav>
+          <ul>
+           <li>
+            <button className='filter-button'>Tümü</button>
+            
+           </li>
+            <li>
+              <button className='filter-button'>Yeni</button>
+            </li>
+
+          </ul>
+        
+        </nav>
       </header>
+      <main className="course-list">
+        {courses.map((course, index) => (
+          <div className="course-card" key={index}>
+
+            
+            <h3>{course.title}</h3>
+            <p>{course.level}</p>
+            {course.trainer && (
+              <div className="trainer">
+                <span>{course.trainer}</span>
+              </div>
+            )}
+            <div className="price">
+              {course.oldPrice && <span className="old-price">{course.oldPrice}</span>}
+              <span className="new-price">{course.price}</span>
+            </div>
+            <button className="enroll-button">KAYDOL</button>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
